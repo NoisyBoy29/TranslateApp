71528b1301ea771b8439fb9be3a932364d045888
Naufal Irawan <irawanaufal29209@gmail.com>
2023-06-09 08:18:29 +0700

    done for SpeechToText.

--- app/src/main/java/com/naufal/translateapp/MainActivity.kt
+++ app/src/main/java/com/naufal/translateapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.naufal.translateapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.text.Editable
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -10,12 +15,14 @@ import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import com.naufal.translateapp.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private var items = arrayOf("English", "Indonesia", "Italy", "France", "Japanese")
     private var conditions = DownloadConditions.Builder().requireWifi().build()
+    private val speechRec = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,15 +45,19 @@ class MainActivity : AppCompatActivity() {
 
             translator.downloadModelIfNeeded(conditions).addOnSuccessListener {
 
-                translator.translate(binding.input.text.toString())
+                translator.translate(binding.tvInput.text.toString())
                     .addOnSuccessListener { translatedText ->
-                        binding.output.text = translatedText
+                        binding.tvOutput.text = translatedText
                     }.addOnFailureListener { exception ->
                         Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
                     }
             }.addOnFailureListener { exception ->
                 Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
             }
+        }
+
+        binding.voiceSpeechButton.setOnClickListener {
+            speechInput()
         }
     }
 
@@ -71,6 +82,27 @@ class MainActivity : AppCompatActivity() {
             "France" -> TranslateLanguage.FRENCH
             "Japanese" -> TranslateLanguage.JAPANESE
             else -> TranslateLanguage.INDONESIAN
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == speechRec && resultCode == Activity.RESULT_OK){
+            val result:ArrayList<String>? = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            binding.tvInput.text = Editable.Factory.getInstance().newEditable(result?.get(0).toString())
+
+        }
+    }
+    private fun speechInput() {
+        if (!SpeechRecognizer.isRecognitionAvailable(this)) {
+            Toast.makeText(this, "Speech Failed", Toast.LENGTH_SHORT).show()
+        } else {
+            val voiceSpeech = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            voiceSpeech.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            voiceSpeech.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            voiceSpeech.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak")
+            startActivityForResult(voiceSpeech,speechRec)
         }
     }
 }
